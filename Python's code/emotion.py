1622f7da6e6dfc0d7c652d4d0ac6dd8bffd6b895
Abdiasarsene <abdiasarsene@gmail.com>
2025-03-04 16:03:24 +0100

    ...é

--- Python's code/emotion.py
+++ Python's code/emotion.py
@@ -44,3 +44,52 @@ plt.ylabel('Number of occurrences')
 plt.title('Distribution of sentiments')
 plt.show()
 
+# Emotion analysis
+def analyze_emotion(text):
+    emotion =NRCLex(text)
+    return emotion.raw_emotion_scores
+
+text['emotion_score']=text['Message_cleaned'].apply(analyze_emotion)
+
+# Extract specific emotion
+def extract_specific_emotion(score):
+    relevant_emotion ={
+        'stress' : score.get('stress', 0),
+        'anticipation': score.get('anticipation',0),
+        'sadness': score.get('sadness',0),
+        'joy' : score.get('joy',0),
+        'fear': score.get('fear',0),
+        'trust' : score.get('trust',0),
+        'disgust' : score.get('disgust',0),
+        'surprise' : score.get('surprise',0),
+        'positive' : score.get('positive',0)
+    }
+    return relevant_emotion
+
+# Apply extracting secific emotion
+text['specific_emotion'] = text['emotion_score'].apply(extract_specific_emotion)
+
+# Create specific emotion as a DataFrame
+emotion_specific = text['specific_emotion'].apply(lambda x: max(x, key=x.get))
+
+# Data viz of distribution of specific emotion
+plt.figure(figsize=(12,5))
+emotion_specific.sum().sort_values(ascending=False).plot(kind='bar', colormap='viridis')
+plt.title('Distribution of emotion')
+plt.xlabel('Emotions')
+plt.ylabel('Frequences')
+
+# Choose emotions with a high score
+text['dominant_emotion'] = text['specific_emotion'].apply(lambda x: max(x, key=x.get) if x else "unknown")
+
+# Compter les émotions par jour
+emotion_trend = text.groupby(['Date', 'dominant_emotion']).size().unstack(fill_value=0)
+
+# Tracer l'évolution des émotions
+emotion_trend.plot(figsize=(14, 6), marker='o')
+plt.title("Évolution des émotions au fil du temps")
+plt.xlabel("Date")
+plt.ylabel("Nombre d'occurrences")
+plt.grid()
+plt.legend(title="Émotions", bbox_to_anchor=(1.05, 1), loc='upper left')
+plt.show()
